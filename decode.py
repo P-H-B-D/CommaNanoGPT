875d4974a0d8e7fe88882040becb77e1dd94078c
Peter Bowman-Davis <pbowmandavis@a16z.com>
2024-12-11 13:25:18 -0800

    feat: decoding and data loading

---
+++ decode.py
@@ -0,0 +1,28 @@
+import torch
+from utils.vqvae import Decoder, CompressorConfig
+import cv2
+
+#Input: [1,129] -> Img
+def decode(input_tensor, device):
+    config = CompressorConfig()
+    with torch.device('cpu'):
+        decoder = Decoder(config)
+        decoder.load_state_dict_from_url('https://huggingface.co/commaai/commavq-gpt2m/resolve/main/decoder_pytorch_model.bin', assign=True)
+        decoder = decoder.eval().to(device='cuda')
+    
+    # Ignore the BOS token
+    target = input_tensor[:, 1:].to(torch.int64).to(device='cpu')
+
+    output = decoder(target)
+    output = output[0]
+    # Convert to float32 before converting to numpy
+    img = output.permute(1, 2, 0).to(torch.float32).cpu().detach().numpy().astype('uint8')
+    return img
+
+# if __name__ == "__main__":
+#     input_tensor = torch.randint(0, 256, (1, 129))
+#     device = torch.device('cpu')
+#     img = decode(input_tensor, device)
+#     print(img.shape)
+#     #save as png
+#     cv2.imwrite("output.png", img)
---
+++ download_bins.py
@@ -0,0 +1,29 @@
+from huggingface_hub import hf_hub_download
+from concurrent.futures import ThreadPoolExecutor
+import os
+
+# Define the repository and file type to download
+REPO_ID = "P-H-B-D-a16z/CommaVQmini"
+FILE_EXTENSION = ".bin"
+DOWNLOAD_DIR = "./nanogpt/"  # Specify download directory
+
+# Ensure download directory exists
+os.makedirs(DOWNLOAD_DIR, exist_ok=True)
+
+# Function to download a single file
+def download_file(file_name):
+    try:
+        # Directly download to DOWNLOAD_DIR without using cache_dir
+        file_path = hf_hub_download(repo_id=REPO_ID, filename=file_name, local_dir=DOWNLOAD_DIR, repo_type="dataset", local_dir_use_symlinks=False)
+        print(f"Downloaded {file_name} to {file_path}")
+    except Exception as e:
+        print(f"Failed to download {file_name}: {e}")
+
+# List the specific files you want to download
+bin_files = [f"{x}.bin" for x in range(6)]
+
+# Download files in parallel
+with ThreadPoolExecutor() as executor:
+    executor.map(download_file, bin_files)
+
+print("All downloads complete.")
